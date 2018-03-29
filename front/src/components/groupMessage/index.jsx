7138708b66c7467d6051498cc0e8a5503a66bb63
huchangfa123 <hcf1095246249@qq.com>
2018-03-29 23:01:41 +0800

    group message css

--- front/src/components/groupMessage/index.jsx
+++ front/src/components/groupMessage/index.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import FloatPanel from '../floatPanel';
 import ui from '../../action/UiAction';
+import './groupMessage.css'
 
 /**
  * 结合floatPanel的群信息右边弹框组件
@@ -18,10 +19,278 @@ export default class GroupMessage extends Component {
     return (
       <FloatPanel title="群信息" show={show}>
         <div className="group-info">
-          <div className="content"></div>
-          <div className="userlist"></div>
+          <div className="content"><span>在线人数:{this.props.curSelectedRoom.get('onlineUsers').length}</span></div>
+          <div className="userlist">
+          {/* {
+            this.props.curSelectedRoom.get('onlineUsers').map((user, index) => (
+              <div className="userItem">
+                <div
+                  key={{index}}
+                  style={{
+                    color: 'white',
+                    width: '40px',
+                    lineHeight: '40px',
+                    textAlign: 'center',
+                    height: '40px',
+                    borderRadius: '50%',
+                    backgroundColor: `${user.get('avatar')}`
+                  }}
+                >
+                {user.get('name').charAt(0)}
+                </div>
+                <span>{user.get('name')}</span>
+              </div>
+            ))
+          } */}
+              <div className="userItem">
+                <div
+                  style={{
+                    color: 'white',
+                    width: '40px',
+                    minWidth: '40px',
+                    minHeight: '40px',
+                    lineHeight: '40px',
+                    textAlign: 'center',
+                    height: '40px',
+                    borderRadius: '50%',
+                    backgroundColor: '#233233'
+                  }}
+                >
+                1
+                </div>
+                <span>222</span>
+              </div>
+              <div className="userItem">
+                <div
+                  style={{
+                    color: 'white',
+                    width: '40px',
+                    lineHeight: '40px',
+                    textAlign: 'center',
+                    height: '40px',
+                    borderRadius: '50%',
+                    backgroundColor: '#233233'
+                  }}
+                >
+                1
+                </div>
+                <span>222</span>
+              </div> <div className="userItem">
+                <div
+                  style={{
+                    color: 'white',
+                    width: '40px',
+                    lineHeight: '40px',
+                    textAlign: 'center',
+                    height: '40px',
+                    borderRadius: '50%',
+                    backgroundColor: '#233233'
+                  }}
+                >
+                1
+                </div>
+                <span>222</span>
+              </div> <div className="userItem">
+                <div
+                  style={{
+                    color: 'white',
+                    width: '40px',
+                    lineHeight: '40px',
+                    textAlign: 'center',
+                    height: '40px',
+                    borderRadius: '50%',
+                    backgroundColor: '#233233'
+                  }}
+                >
+                1
+                </div>
+                <span>222</span>
+              </div> <div className="userItem">
+                <div
+                  style={{
+                    color: 'white',
+                    width: '40px',
+                    lineHeight: '40px',
+                    textAlign: 'center',
+                    height: '40px',
+                    borderRadius: '50%',
+                    backgroundColor: '#233233'
+                  }}
+                >
+                1
+                </div>
+                <span>222</span>
+              </div> <div className="userItem">
+                <div
+                  style={{
+                    color: 'white',
+                    width: '40px',
+                    lineHeight: '40px',
+                    textAlign: 'center',
+                    height: '40px',
+                    borderRadius: '50%',
+                    backgroundColor: '#233233'
+                  }}
+                >
+                1
+                </div>
+                <span>222</span>
+              </div> <div className="userItem">
+                <div
+                  style={{
+                    color: 'white',
+                    width: '40px',
+                    lineHeight: '40px',
+                    textAlign: 'center',
+                    height: '40px',
+                    borderRadius: '50%',
+                    backgroundColor: '#233233'
+                  }}
+                >
+                1
+                </div>
+                <span>222</span>
+              </div> <div className="userItem">
+                <div
+                  style={{
+                    color: 'white',
+                    width: '40px',
+                    lineHeight: '40px',
+                    textAlign: 'center',
+                    height: '40px',
+                    borderRadius: '50%',
+                    backgroundColor: '#233233'
+                  }}
+                >
+                1
+                </div>
+                <span>222</span>
+              </div> <div className="userItem">
+                <div
+                  style={{
+                    color: 'white',
+                    width: '40px',
+                    lineHeight: '40px',
+                    textAlign: 'center',
+                    height: '40px',
+                    borderRadius: '50%',
+                    backgroundColor: '#233233'
+                  }}
+                >
+                1
+                </div>
+                <span>222</span>
+              </div> <div className="userItem">
+                <div
+                  style={{
+                    color: 'white',
+                    width: '40px',
+                    lineHeight: '40px',
+                    textAlign: 'center',
+                    height: '40px',
+                    borderRadius: '50%',
+                    backgroundColor: '#233233'
+                  }}
+                >
+                1
+                </div>
+                <span>222</span>
+              </div> <div className="userItem">
+                <div
+                  style={{
+                    color: 'white',
+                    width: '40px',
+                    lineHeight: '40px',
+                    textAlign: 'center',
+                    height: '40px',
+                    borderRadius: '50%',
+                    backgroundColor: '#233233'
+                  }}
+                >
+                1
+                </div>
+                <span>222</span>
+              </div> <div className="userItem">
+                <div
+                  style={{
+                    color: 'white',
+                    width: '40px',
+                    lineHeight: '40px',
+                    textAlign: 'center',
+                    height: '40px',
+                    borderRadius: '50%',
+                    backgroundColor: '#233233'
+                  }}
+                >
+                1
+                </div>
+                <span>222</span>
+              </div> <div className="userItem">
+                <div
+                  style={{
+                    color: 'white',
+                    width: '40px',
+                    lineHeight: '40px',
+                    textAlign: 'center',
+                    height: '40px',
+                    borderRadius: '50%',
+                    backgroundColor: '#233233'
+                  }}
+                >
+                1
+                </div>
+                <span>222</span>
+              </div> <div className="userItem">
+                <div
+                  style={{
+                    color: 'white',
+                    width: '40px',
+                    lineHeight: '40px',
+                    textAlign: 'center',
+                    height: '40px',
+                    borderRadius: '50%',
+                    backgroundColor: '#233233'
+                  }}
+                >
+                1
+                </div>
+                <span>222</span>
+              </div> <div className="userItem">
+                <div
+                  style={{
+                    color: 'white',
+                    width: '40px',
+                    lineHeight: '40px',
+                    textAlign: 'center',
+                    height: '40px',
+                    borderRadius: '50%',
+                    backgroundColor: '#233233'
+                  }}
+                >
+                1
+                </div>
+                <span>222</span>
+              </div> <div className="userItem">
+                <div
+                  style={{
+                    color: 'white',
+                    width: '40px',
+                    lineHeight: '40px',
+                    textAlign: 'center',
+                    height: '40px',
+                    borderRadius: '50%',
+                    backgroundColor: '#233233'
+                  }}
+                >
+                1
+                </div>
+                <span>222</span>
+              </div>
+          </div>
         </div>
-        <div className="group-exit"></div>
+        <div className="group-exit">
+          <button>退出群组</button>
+        </div>
       </FloatPanel>
     );
   }
