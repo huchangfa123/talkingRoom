cd6744692fc9a1a74457dc91e8861badd0efa8cf
huchangfa123 <hcf1095246249@qq.com>
2018-04-07 15:35:06 +0800

    webpack build prove

--- front/webpack.config.babel.js
+++ front/webpack.config.babel.js
@@ -1,5 +1,7 @@
 import path from 'path';
-console.log(path.join(__dirname, 'public'))
+import UglifyJSPlugin from 'uglifyjs-webpack-plugin';
+import webpack from 'webpack'
+
 module.exports = {
   devtool: 'eval-source-map',
   entry: {
@@ -7,12 +9,14 @@ module.exports = {
       'babel-polyfill',
       'webpack-dev-server/client?http://localhost:8080/',
       './src/index.jsx'
-    ]
+    ],
+    vendor: ['react', 'react-dom', 'react-router-dom', 'immutable']
   },
   output: {
     path: path.join(__dirname, 'public/static'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
+    publicPath: '/static/',
+    filename: '[name].[chunkhash:4].js',
+    chunkFilename: '[name].[chunkhash:4].child.js'
   },
   devServer: {
     contentBase: path.join(__dirname, 'public'),
@@ -41,6 +45,43 @@ module.exports = {
       }
     ]
   },
+  plugins: [
+    new UglifyJSPlugin({
+      uglifyOptions: {
+        ie8: false,
+        output: {
+          comments: false,
+          beautify: false,
+        },
+        mangle: {
+          keep_fnames: true
+        },
+        compress: {
+          warnings: false,
+          drop_console: true
+        },
+      }
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: ['vendor', 'runtime'], //把公共资源抽离，避免多次打包
+      minChunks: Infinity,
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      // ( 公共chunk(commnons chunk) 的名称)
+      name: "commons",
+      // ( 公共chunk 的文件名)
+      filename: "commons.[chunkhash:4].js",
+      // (模块必须被 3个 入口chunk 共享)
+      minChunks: 3
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      // (选择所有被选 chunks 的子 chunks)
+      children: true,
+      async: true,
+      // (在提取之前需要至少三个子 chunk 共享这个模块)
+      minChunks: 3,
+    })
+  ],
   resolve: {
     extensions: ['.js', '.jsx']
   },
